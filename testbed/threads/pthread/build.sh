9c79a823a59584d1b2c52d3fdf4e6beb0b4342ee
Ravi Sankar Jonnal <rjonnal@gmail.com>
2018-01-05 16:27:27 -0800

    many changes

---
+++ testbed/threads/pthread/build.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+gcc multithread.c -lpthread -Wimplicit-function-declaration
---
+++ testbed/threads/pthread/multithread.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+long foo;
+long bar;
+void sleep(int x);
+
+// A normal C function that is executed as a thread 
+// when its name is specified in pthread_create()
+void *myThreadFun(void *vargp)
+{
+    sleep(1.5);
+    printf("Printing GeeksQuiz from Thread \n");
+    return NULL;
+}
+  
+int main()
+{
+    pthread_t tid;
+    printf("Before Thread\n");
+    pthread_create(&tid, NULL, myThreadFun, NULL);
+    pthread_join(tid, NULL);
+    printf("After Thread\n");
+    exit(0);
+}
